821bf4f3a3df99d45287f8aa2988d0a489073aa6
caijynb <1332794849@qq.com>
2021-10-13 20:57:58 +0800

    添加是否已安装的检查

--- goinstall.sh
+++ goinstall.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 wget -c https://golang.org/dl/go1.17.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-sed -i "2a\export PATH=\$PATH:/usr/local/go/bin" /etc/profile
-source /etc/profile
+if ! go version;then
+  sed -i "2a\export PATH=\$PATH:/usr/local/go/bin" /etc/profile
+  sleep 1
+  source /etc/profile
+else
+  echo "您早已安装过go了~"
+fi
