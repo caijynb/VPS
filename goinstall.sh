fff9930b1e5c87f49d4af8cc9f7d2fad8854b0fd
caijynb <1332794849@qq.com>
2021-10-13 20:49:29 +0800

    转移$符

--- goinstall.sh
+++ goinstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 wget -c https://golang.org/dl/go1.17.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-sed -i "2a\export PATH=$PATH:/usr/local/go/bin" /etc/profile
+sed -i "2a\export PATH=\$PATH:/usr/local/go/bin" /etc/profile
 source /etc/profile
